075b928958b1fb6dc3a2b54e91b19996a7439052
Thom <thomvanoorschot@hotmail.com>
2025-04-20 16:37:32 +0200

    Added imgui and implot

--- client/src/main.zig
+++ client/src/main.zig
@@ -1,24 +1,24 @@
 const std = @import("std");
 const xev = @import("xev");
 const Loop = xev.Loop;
-const App = @import("visualization/app.zig").App;
+// const App = @import("visualization/app.zig").App;
 const window_title = "zigma_client";
 
 pub fn main() !void {
     var loop = try Loop.init(.{});
     defer loop.deinit();
-    var allocator_state = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = allocator_state.allocator();
+    // var allocator_state = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator = allocator_state.allocator();
 
-    var app = try App.init(
-        allocator,
-        &loop,
-        window_title,
-        1920,
-        1080,
-    );
-    app.start();
+    // var app = try App.init(
+    //     allocator,
+    //     &loop,
+    //     window_title,
+    //     1920,
+    //     1080,
+    // );
+    // app.start();
 
 
-    try loop.run(.until_done);
+    // try loop.run(.until_done);
 }

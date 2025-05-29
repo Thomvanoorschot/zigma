6eba0b29e48ba2719937208f50c900972a1a9f61
Thom <thomvanoorschot@hotmail.com>
2025-05-29 20:30:04 +0200

    Starting to render using zignite

--- client/src/main.zig
+++ client/src/main.zig
@@ -1,15 +1,111 @@
-const zignite = @import("zignite");
 const std = @import("std");
+const zignite = @import("zignite");
+const ob = @import("visualization/orderbook.zig");
+const shared_models = @import("shared_models");
+
+const pthread = zignite.pthread;
+const websocket = zignite.websocket;
+const emscripten_utils = zignite.emscripten_utils;
 const imgui = zignite.imgui;
 const engine = zignite.engine;
-const App = @import("visualization/app.zig").App;
-const window_title = "zigma_client";
+const websocket_web_worker = zignite.websocket_web_worker;
+const WebSocketWebWorker = websocket_web_worker.WebSocketWebWorker;
 
+const parseOrderbook = shared_models.parseOrderbook;
+const OrderbookWindows = ob.OrderbookWindows;
+const SharedData = struct {
+    orderbook_windows: *OrderbookWindows,
+};
+
+fn workerEntrypoint(web_worker: *WebSocketWebWorker(SharedData)) !void {
+    _ = web_worker;
+}
+
+fn onOpenCallback(web_worker: *WebSocketWebWorker(SharedData)) !bool {
+    try web_worker.std_out.print("WebSocket opened\n", .{});
+    if (web_worker.open_socket) |open_socket| {
+        _ = websocket.sendText(open_socket, "open_orderbook:BTC/USD");
+    }
+    return true;
+}
+
+// fn onMessageCallback(web_worker: *WebSocketWebWorker(SharedData), message: []const u8) !bool {
+//     try web_worker.std_out.print("Received message: {s}\n", .{message});
+//     const orderbook = parseOrderbook(web_worker.allocator, message) catch |err| {
+//         try web_worker.std_err.print("Failed to parse orderbook data: {s}\n", .{@errorName(err)});
+//         return error.FailedToParseOrderbook;
+//     };
+//     try web_worker.std_out.print("Parsed orderbook: {s}\n", .{orderbook.ticker});
+//     // try web_worker.std_out.print("Parsed orderbook: {s}\n", .{orderbook.ticker});
+
+//     // if (self.windows.?.get(ob.ticker)) |window| {
+//     //     if (window.popen) {
+//     //         window.orderbook = ob;
+//     //     } else {
+//     //         self.allocator.free(window.open_message);
+//     //         self.allocator.destroy(window);
+//     //         _ = self.windows.?.remove(ob.ticker);
+//     //         // TODO This is still dangling
+//     //         self.tcp_client.?.write(window.close_message);
+//     //     }
+//     // } else {}
+//     return true;
+// }
+
+fn onMessageCallback(web_worker: *WebSocketWebWorker(SharedData), message: []const u8) !bool {
+    web_worker.std_out.print("Received message ({d} bytes): ", .{message.len}) catch unreachable;
+    for (message[0..@min(message.len, 50)]) |byte| {
+        web_worker.std_out.print("{x:0>2} ", .{byte}) catch unreachable;
+    }
+    web_worker.std_out.print("\n", .{}) catch unreachable;
+    const orderbook = parseOrderbook(std.heap.c_allocator, message) catch {
+        try web_worker.std_out.print("Failed\n", .{});
+        return true;
+    };
+    // _ = web_worker;
+    _ = orderbook;
+    // web_worker.std_out.print("Parsed orderbook: {s}\n", .{orderbook.ticker}) catch {
+    //     return true;
+    // };
+    return true;
+}
+
+fn onErrorCallback(web_worker: *WebSocketWebWorker(SharedData)) !bool {
+    _ = web_worker;
+    return true;
+}
+
+fn onCloseCallback(web_worker: *WebSocketWebWorker(SharedData), code: u16, reason: []const u8) !bool {
+    _ = web_worker;
+    _ = code;
+    _ = reason;
+    return true;
+}
 pub fn main() !void {
+    const allocator = std.heap.c_allocator;
+    var shared = SharedData{
+        .orderbook_windows = try OrderbookWindows.init(allocator),
+    };
+
+    // Create web worker
+    const ww = try WebSocketWebWorker(SharedData).init(
+        std.heap.c_allocator,
+        "ws://127.0.0.1:8081",
+        &shared,
+        workerEntrypoint,
+        .{
+            .on_open_cb = onOpenCallback,
+            .on_message_cb = onMessageCallback,
+            .on_error_cb = onErrorCallback,
+            .on_close_cb = onCloseCallback,
+        },
+    );
+    defer std.heap.c_allocator.destroy(ww);
+
+    // Main thread loop
     var e = try engine.Engine.init(.{
-        .width = 3456,
-        .height = 2234,
-        .with_implot = false,
+        .width = 1024,
+        .height = 768,
     });
     defer e.deinit();
 
--- client/src/visualization/orderbook.zig
+++ client/src/visualization/orderbook.zig
@@ -1,198 +1,195 @@
-// const std = @import("std");
-// const gui = @import("../gui.zig");
-// const plot = @import("../plot.zig");
-// const glfw = @import("zglfw");
-// const shared_models = @import("shared_models");
-// const async_zocket = @import("async_zocket");
-// const app = @import("app.zig");
+const std = @import("std");
 
-// const parseOrderbook = shared_models.parseOrderbook;
+const zignite = @import("zignite");
+const gui = zignite.imgui;
+const plot = zignite.implot;
+const glfw = zignite.glfw;
+const shared_models = @import("shared_models");
 
-// const Client = async_zocket.Client;
-// const OrderBook = shared_models.OrderBook;
-// const PriceLevel = shared_models.PriceLevel;
-// const MessageTypes = app.MessageTypes;
+const parseOrderbook = shared_models.parseOrderbook;
 
-// var popen: bool = true;
+const OrderBook = shared_models.OrderBook;
+const PriceLevel = shared_models.PriceLevel;
 
-// const OrderbookWindow = struct {
-//     ticker: []const u8,
-//     open_message: [:0]u8,
-//     close_message: [:0]u8,
-//     popen: bool = false,
-//     orderbook: ?*const OrderBook = null,
-// };
+var popen: bool = true;
 
-// pub const OrderbookWindows = struct {
-//     allocator: std.mem.Allocator,
-//     windows: ?std.StringHashMap(*OrderbookWindow) = null,
-//     wss_client: ?*Client = null,
-//     const Self = @This();
+const OrderbookWindow = struct {
+    ticker: []const u8,
+    open_message: [:0]u8,
+    close_message: [:0]u8,
+    popen: bool = false,
+    orderbook: ?*const OrderBook = null,
+};
 
-//     pub fn init(
-//         allocator: std.mem.Allocator,
-//     ) !*OrderbookWindows {
-//         const self = try allocator.create(Self);
-//         self.* = .{
-//             .allocator = allocator,
-//             .windows = std.StringHashMap(*OrderbookWindow).init(allocator),
-//         };
-//         return self;
-//     }
+pub const OrderbookWindows = struct {
+    allocator: std.mem.Allocator,
+    windows: ?std.StringHashMap(*OrderbookWindow) = null,
+    const Self = @This();
 
-//     pub fn deinit(self: *Self) void {
-//         var it = self.windows.?.valueIterator();
-//         while (it.next()) |window| {
-//             self.allocator.free(window.*.open_message);
-//             self.allocator.free(window.*.close_message);
-//             self.allocator.destroy(window.*);
-//         }
-//         self.windows.?.deinit();
-//     }
+    pub fn init(
+        allocator: std.mem.Allocator,
+    ) !*OrderbookWindows {
+        const self = try allocator.create(Self);
+        self.* = .{
+            .allocator = allocator,
+            .windows = std.StringHashMap(*OrderbookWindow).init(allocator),
+        };
+        return self;
+    }
 
-//     pub fn openWindow(
-//         self: *Self,
-//         ticker: []const u8,
-//     ) !void {
-//         const window = try self.allocator.create(OrderbookWindow);
-//         const open_msg = std.fmt.allocPrintZ(self.allocator, "open_orderbook:{s}", .{ticker}) catch unreachable;
-//         const close_msg = std.fmt.allocPrintZ(self.allocator, "close_orderbook:{s}", .{ticker}) catch unreachable;
-//         window.* = .{
-//             .ticker = ticker,
-//             .open_message = open_msg,
-//             .close_message = close_msg,
-//             .popen = true,
-//         };
-//         try self.windows.?.put(ticker, window);
-//         try self.wss_client.?.write(open_msg);
-//     }
+    pub fn deinit(self: *Self) void {
+        var it = self.windows.?.valueIterator();
+        while (it.next()) |window| {
+            self.allocator.free(window.*.open_message);
+            self.allocator.free(window.*.close_message);
+            self.allocator.destroy(window.*);
+        }
+        self.windows.?.deinit();
+    }
 
-//     pub fn orderbookCallback(
-//         self_: *anyopaque,
-//         payload: []const u8,
-//     ) anyerror!void {
-//         const self = @as(*Self, @ptrCast(@alignCast(self_)));
-//         const ob = parseOrderbook(self.allocator, payload) catch |err| {
-//             std.log.err("Failed to parse orderbook data: {s}", .{@errorName(err)});
-//             return error.FailedToParseOrderbook;
-//         };
-//         if (self.windows.?.get(ob.ticker)) |window| {
-//             if (window.popen) {
-//                 window.orderbook = ob;
-//             } else {
-//                 self.allocator.free(window.open_message);
-//                 self.allocator.destroy(window);
-//                 _ = self.windows.?.remove(ob.ticker);
-//                 // TODO This is still dangling
-//                 try self.wss_client.?.write(window.close_message);
-//             }
-//         } else {}
-//     }
+    pub fn openWindow(
+        self: *Self,
+        ticker: []const u8,
+    ) !void {
+        const window = try self.allocator.create(OrderbookWindow);
+        const open_msg = std.fmt.allocPrintZ(self.allocator, "open_orderbook:{s}", .{ticker}) catch unreachable;
+        const close_msg = std.fmt.allocPrintZ(self.allocator, "close_orderbook:{s}", .{ticker}) catch unreachable;
+        window.* = .{
+            .ticker = ticker,
+            .open_message = open_msg,
+            .close_message = close_msg,
+            .popen = true,
+        };
+        try self.windows.?.put(ticker, window);
+        // try self.wss_client.?.write(open_msg);
+    }
 
-//     pub fn plot(self: *Self) !void {
-//         if (self.windows == null) {
-//             return;
-//         }
+    pub fn orderbookCallback(
+        self_: *anyopaque,
+        payload: []const u8,
+    ) anyerror!void {
+        const self = @as(*Self, @ptrCast(@alignCast(self_)));
+        const ob = parseOrderbook(self.allocator, payload) catch |err| {
+            std.log.err("Failed to parse orderbook data: {s}", .{@errorName(err)});
+            return error.FailedToParseOrderbook;
+        };
+        if (self.windows.?.get(ob.ticker)) |window| {
+            if (window.popen) {
+                window.orderbook = ob;
+            } else {
+                self.allocator.free(window.open_message);
+                self.allocator.destroy(window);
+                _ = self.windows.?.remove(ob.ticker);
+                // TODO This is still dangling
+                // try self.wss_client.?.write(window.close_message);
+            }
+        } else {}
+    }
 
-//         var it = self.windows.?.valueIterator();
-//         while (it.next()) |window| {
-//             if (window.*.orderbook != null) {
-//                 try plotOrderbookWindow(window.*);
-//             }
-//         }
-//     }
+    pub fn plot(self: *Self) !void {
+        if (self.windows == null) {
+            return;
+        }
 
-//     fn plotOrderbookWindow(window: *OrderbookWindow) !void {
-//         var title_buf: [128]u8 = undefined;
-//         const orderbook = window.orderbook.?;
-//         const title = std.fmt.bufPrintZ(&title_buf, "Order Book - {s} ({s})", .{ orderbook.ticker, orderbook.exchange }) catch unreachable;
+        var it = self.windows.?.valueIterator();
+        while (it.next()) |window| {
+            if (window.*.orderbook != null) {
+                try plotOrderbookWindow(window.*);
+            }
+        }
+    }
 
-//         if (popen and gui.begin(title, .{ .popen = &window.popen })) {
-//             defer gui.end();
+    fn plotOrderbookWindow(window: *OrderbookWindow) !void {
+        var title_buf: [128]u8 = undefined;
+        const orderbook = window.orderbook.?;
+        const title = std.fmt.bufPrintZ(&title_buf, "Order Book - {s} ({s})", .{ orderbook.ticker, orderbook.exchange }) catch unreachable;
 
-//             const bids = orderbook.bids;
-//             const asks = orderbook.asks;
+        if (popen and gui.begin(title, .{ .popen = &window.popen })) {
+            defer gui.end();
 
-//             var max_vol: f64 = 0.0;
-//             for (bids.items) |bid| max_vol = @max(max_vol, bid[1]);
-//             for (asks.items) |ask| max_vol = @max(max_vol, ask[1]);
+            const bids = orderbook.bids;
+            const asks = orderbook.asks;
 
-//             if (max_vol > 0) {
-//                 max_vol *= 1.05;
-//             } else {
-//                 max_vol = 1.0;
-//             }
+            var max_vol: f64 = 0.0;
+            for (bids.items) |bid| max_vol = @max(max_vol, bid[1]);
+            for (asks.items) |ask| max_vol = @max(max_vol, ask[1]);
 
-//             const table_flags = gui.TableFlags{
-//                 .borders = .{ .inner_v = true },
-//                 .sizing = .fixed_fit,
-//                 .row_bg = true,
-//             };
+            if (max_vol > 0) {
+                max_vol *= 1.05;
+            } else {
+                max_vol = 1.0;
+            }
 
-//             const text_buf_size = 64;
-//             var text_buf: [text_buf_size]u8 = undefined;
+            const table_flags = gui.TableFlags{
+                .borders = .{ .inner_v = true },
+                .sizing = .fixed_fit,
+                .row_bg = true,
+            };
 
-//             if (gui.beginTable("AsksTable", .{ .column = 2, .flags = table_flags })) {
-//                 defer gui.endTable();
+            const text_buf_size = 64;
+            var text_buf: [text_buf_size]u8 = undefined;
 
-//                 gui.tableSetupColumn("Price", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
-//                 gui.tableSetupColumn("Volume", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
+            if (gui.beginTable("AsksTable", .{ .column = 2, .flags = table_flags })) {
+                defer gui.endTable();
 
-//                 gui.tableNextRow(.{});
-//                 _ = gui.tableSetColumnIndex(0);
-//                 gui.textUnformattedColored(0xFF0000FF, "Asks");
+                gui.tableSetupColumn("Price", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
+                gui.tableSetupColumn("Volume", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
 
-//                 gui.tableHeadersRow();
+                gui.tableNextRow(.{});
+                _ = gui.tableSetColumnIndex(0);
+                gui.textUnformattedColored(0xFF0000FF, "Asks");
 
-//                 var ask_cum_vol: f64 = 0;
-//                 for (asks.items, 0..) |_, i| {
-//                     const ask = asks.items[asks.items.len - 1 - i];
-//                     ask_cum_vol += ask[1];
+                gui.tableHeadersRow();
 
-//                     gui.tableNextRow(.{});
+                var ask_cum_vol: f64 = 0;
+                for (asks.items, 0..) |_, i| {
+                    const ask = asks.items[asks.items.len - 1 - i];
+                    ask_cum_vol += ask[1];
 
-//                     _ = gui.tableSetColumnIndex(0);
-//                     const price_fmt = std.fmt.bufPrint(&text_buf, "{d:.2}", .{ask[0]}) catch "ERR";
-//                     // Colors are AABBGGRR
-//                     gui.textUnformattedColored(0xFF0000FF, price_fmt);
+                    gui.tableNextRow(.{});
 
-//                     _ = gui.tableSetColumnIndex(1);
-//                     const vol_fmt = std.fmt.bufPrint(&text_buf, "{d:.3}", .{ask[1]}) catch "ERR";
-//                     gui.textUnformattedColored(0xFF0000FF, vol_fmt);
-//                 }
-//             }
+                    _ = gui.tableSetColumnIndex(0);
+                    const price_fmt = std.fmt.bufPrint(&text_buf, "{d:.2}", .{ask[0]}) catch "ERR";
+                    // Colors are AABBGGRR
+                    gui.textUnformattedColored(0xFF0000FF, price_fmt);
 
-//             gui.separator();
-//             const spread_val = if (asks.items.len > 0 and bids.items.len > 0) asks.items[0][0] - bids.items[0][0] else 0.0;
-//             const spread_text = std.fmt.bufPrint(&text_buf, "Spread: {d:.2}", .{spread_val}) catch "ERR SPREAD";
-//             gui.textUnformatted(spread_text);
-//             gui.separator();
+                    _ = gui.tableSetColumnIndex(1);
+                    const vol_fmt = std.fmt.bufPrint(&text_buf, "{d:.3}", .{ask[1]}) catch "ERR";
+                    gui.textUnformattedColored(0xFF0000FF, vol_fmt);
+                }
+            }
 
-//             if (gui.beginTable("BidsTable", .{ .column = 2, .flags = table_flags })) {
-//                 defer gui.endTable();
+            gui.separator();
+            const spread_val = if (asks.items.len > 0 and bids.items.len > 0) asks.items[0][0] - bids.items[0][0] else 0.0;
+            const spread_text = std.fmt.bufPrint(&text_buf, "Spread: {d:.2}", .{spread_val}) catch "ERR SPREAD";
+            gui.textUnformatted(spread_text);
+            gui.separator();
 
-//                 gui.tableSetupColumn("Price", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
-//                 gui.tableSetupColumn("Volume", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
-//                 gui.tableNextRow(.{});
-//                 _ = gui.tableSetColumnIndex(0);
-//                 gui.textUnformattedColored(0xFF00FF00, "Bids");
+            if (gui.beginTable("BidsTable", .{ .column = 2, .flags = table_flags })) {
+                defer gui.endTable();
 
-//                 gui.tableHeadersRow();
+                gui.tableSetupColumn("Price", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
+                gui.tableSetupColumn("Volume", .{ .flags = .{ .width_fixed = true }, .init_width_or_height = 80.0 });
+                gui.tableNextRow(.{});
+                _ = gui.tableSetColumnIndex(0);
+                gui.textUnformattedColored(0xFF00FF00, "Bids");
 
-//                 var bid_cum_vol: f64 = 0;
-//                 for (bids.items) |bid| {
-//                     bid_cum_vol += bid[1];
+                gui.tableHeadersRow();
 
-//                     gui.tableNextRow(.{});
+                var bid_cum_vol: f64 = 0;
+                for (bids.items) |bid| {
+                    bid_cum_vol += bid[1];
 
-//                     _ = gui.tableSetColumnIndex(0);
-//                     const price_fmt = std.fmt.bufPrint(&text_buf, "{d:.2}", .{bid[0]}) catch "ERR";
-//                     gui.textUnformattedColored(0xFF00FF00, price_fmt);
-//                     _ = gui.tableSetColumnIndex(1);
-//                     const vol_fmt = std.fmt.bufPrint(&text_buf, "{d:.3}", .{bid[1]}) catch "ERR";
-//                     gui.textUnformattedColored(0xFF00FF00, vol_fmt);
-//                 }
-//             }
-//         }
-//     }
-// };
+                    gui.tableNextRow(.{});
+
+                    _ = gui.tableSetColumnIndex(0);
+                    const price_fmt = std.fmt.bufPrint(&text_buf, "{d:.2}", .{bid[0]}) catch "ERR";
+                    gui.textUnformattedColored(0xFF00FF00, price_fmt);
+                    _ = gui.tableSetColumnIndex(1);
+                    const vol_fmt = std.fmt.bufPrint(&text_buf, "{d:.3}", .{bid[1]}) catch "ERR";
+                    gui.textUnformattedColored(0xFF00FF00, vol_fmt);
+                }
+            }
+        }
+    }
+};
